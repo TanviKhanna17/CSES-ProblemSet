8fe02e7cf9bdd1596f06c8fb72afeb26dfd3f3c3
TanviKhanna17 <76871243+TanviKhanna17@users.noreply.github.com>
2024-12-25 22:29:54 +0530

    BitStrings and Trailing Zeroes

---
+++ INTRODUCTORY PROBLEMS/Bit_Strings.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi;  
+typedef vector<ll> vl; 
+typedef vector<vl> vvl; 
+
+#define f(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
+#define fn(a, b, c) for (int(a) = (b); (a) >= (c); --(a)) 
+#define fit(a, b) for (auto&(a) : (b)) 
+
+#define rep(i, n) f(i, 0, n) 
+#define repn(i, n) fn(i, 1, n) 
+#define SQR(x) ((LL)(x) * (x)) 
+
+#define i(a,n) rep(i,n) cin>>a[i]
+#define o(a,n) rep(i,n) cout<<a[i]<<" " ; cout<<endl;
+
+#define ALL(v) v.begin(), v.end() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define TC(t) while (t--) 
+#define mod (ll)(1e9 + 7)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // int t;
+    // cin>>t;
+    // TC(t)
+    // {
+        
+    // }
+
+    int n;
+    cin>>n;
+    
+    //long long ans = pow(2,n); // uses floating point and losts estimation, cannot be used;
+    long long ans = 2;
+    rep(i,n-1)
+    {
+        ans = (ans*2)%mod;
+    }
+    cout<<ans<<'\n';
+    return 0;
+}
